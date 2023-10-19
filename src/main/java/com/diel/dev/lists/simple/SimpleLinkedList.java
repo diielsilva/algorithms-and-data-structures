b3519873cbf38a3041fd99eea34db96cdc5c4f7f
Daniel Silva de Sousa <diielsilvasousa@gmail.com>
2023-10-19 15:57:02 -0300

    adding a simple linked list

---
+++ src/main/java/com/diel/dev/lists/simple/SimpleLinkedList.java
@@ -0,0 +1,71 @@
+package com.diel.dev.lists.simple;
+
+public class SimpleLinkedList {
+    private SimpleListNode root;
+    private Integer length;
+
+    public SimpleLinkedList() {
+        this.length = 0;
+    }
+
+    public void insert(Integer value) {
+        SimpleListNode node = new SimpleListNode(value);
+
+        if (isEmpty()) {
+            root = node;
+            length++;
+        } else {
+            SimpleListNode aux = root;
+
+            while (aux.getNext() != null) {
+                aux = aux.getNext();
+            }
+
+            aux.setNext(node);
+            length++;
+        }
+    }
+
+    public Integer remove(Integer index) {
+        if (isEmpty()) {
+            return -1;
+        }
+
+        int count = 0;
+
+        SimpleListNode aux = root;
+
+        if (index == 0) {
+            length--;
+            root = root.getNext();
+            return aux.getValue();
+        }
+
+        SimpleListNode previous = aux;
+
+        while (aux != null) {
+            previous = aux;
+            aux = aux.getNext();
+            count++;
+            if (count == index) {
+                length--;
+                previous.setNext(aux.getNext());
+                return aux.getValue();
+            }
+        }
+
+        return -1;
+    }
+
+    public SimpleListNode getRoot() {
+        return this.root;
+    }
+
+    public Integer size() {
+        return length;
+    }
+
+    public boolean isEmpty() {
+        return root == null;
+    }
+}
---
+++ src/test/java/com/diel/dev/lists/simple/SimpleLinkedListTest.java
@@ -0,0 +1,62 @@
+package com.diel.dev.lists.simple;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class SimpleLinkedListTest {
+    private SimpleLinkedList linkedList;
+
+    @BeforeEach
+    void setup() {
+        linkedList = new SimpleLinkedList();
+        linkedList.insert(10);
+        linkedList.insert(15);
+        linkedList.insert(20);
+        linkedList.insert(25);
+    }
+
+    @Test
+    void assertThatValuesWasCorrectlyInserted() {
+        assertEquals(10, linkedList.getRoot().getValue());
+        assertEquals(15, linkedList.getRoot().getNext().getValue());
+        assertEquals(20, linkedList.getRoot().getNext().getNext().getValue());
+        assertEquals(25, linkedList.getRoot().getNext().getNext().getNext().getValue());
+        assertEquals(4, linkedList.size());
+    }
+
+    @Test
+    void assertThatValuesWasCorrectlyRemoved() {
+        Integer value = linkedList.remove(10);
+
+        assertEquals(-1, value);
+        assertEquals(4, linkedList.size());
+
+        value = linkedList.remove(0);
+
+        assertEquals(10, value);
+        assertEquals(3, linkedList.size());
+        assertEquals(15, linkedList.getRoot().getValue());
+
+        value = linkedList.remove(1);
+
+        assertEquals(20, value);
+        assertEquals(2, linkedList.size());
+        assertEquals(15, linkedList.getRoot().getValue());
+        assertEquals(25, linkedList.getRoot().getNext().getValue());
+
+        value = linkedList.remove(1);
+
+        assertEquals(25, value);
+        assertEquals(1, linkedList.size());
+        assertEquals(15, linkedList.getRoot().getValue());
+
+        value = linkedList.remove(0);
+
+        assertEquals(15, value);
+        assertTrue(linkedList.isEmpty());
+
+    }
+}
